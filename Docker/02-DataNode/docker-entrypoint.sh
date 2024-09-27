10ea0640cb546ff788237c5d83df2cb9c98b0ae6
Dmitry Slobodchikov <d.slobodchikov@intraclear.com>
2024-09-27 15:39:19 +0300

    Add data node image

---
+++ Docker/02-DataNode/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR=$(pwd)
+CONFIG=/etc/my.cnf
+PATH=$CURRENT_DIR:$PATH
+THRESHOLD=9
+CNT=0
+
+if [[ $1 == "ndbd" && -f $CONFIG ]]; then
+    while true; do
+        /usr/sbin/$1 \
+            --foreground \
+            --nodaemon
+        CNT=$((CNT+1))
+        sleep 10
+        [[ $CNT -gt $THRESHOLD ]] && break
+    done
+fi
+
+cat <<EOM
+
+--- Caution! ---
+The MySQL NDB Data Node stopped after 10 attempts or due to wrong arguments.
+
+If you did not use health checks or readiness probes, you may want to investigate the pod or container.
+Currently, the runtime is stuck on the command "tail -f /dev/null."
+
+Good luck!
+----------------
+EOM
+
+tail -f /dev/null # for debug purpose
