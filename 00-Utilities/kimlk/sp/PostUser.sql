94e50ac5c170e072c6a5440841a2d6551a2a3adb
Dmitry Slobodchikov <d.slobodchikov@intraclear.com>
2024-10-07 15:46:21 +0300

    Add MySQL grab utilities

---
+++ 00-Utilities/kimlk/sp/PostUser.sql
@@ -0,0 +1,38 @@
+DROP PROCEDURE IF EXISTS kimlk.PostUser;
+DELIMITER $$
+$$
+CREATE PROCEDURE kimlk.PostUser(
+	_email VARCHAR(256),
+	_user JSON,
+	_id JSON,
+	_auth JSON
+)
+BEGIN
+    DECLARE _errno INT;
+    DECLARE _errmsg VARCHAR(256);
+
+    START TRANSACTION;
+        INSERT IGNORE INTO users (
+            `email`,
+            `user`, 
+            `id`, 
+            `auth`
+        ) VALUES (
+            _email,
+            _user,
+            _id,
+            _auth
+        )
+        ;
+        GET CURRENT DIAGNOSTICS CONDITION 1 _errno = MYSQL_ERRNO, _errmsg = MESSAGE_TEXT;
+
+    IF (_errno > 0) THEN
+        SET @error_message_text = CONCAT('An error occurred: ', _errno, ', Message: ', _errmsg);
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = @error_message_text;
+        ROLLBACK;
+    ELSE
+        COMMIT;
+    END IF;
+END
+$$
+DELIMITER ;
